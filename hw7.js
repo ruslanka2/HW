6912c97c83544be13c642ca1896beaef70f67a39
Ruslan <ruslan_feoktistov@mail.ru>
2023-08-10 16:44:11 +0400

    add

--- hw7.js
+++ hw7.js
@@ -40,3 +40,59 @@ const employee = new Employee("John Smith");
 employee.displayInfo();
 const manager = new Manager("Jane Doe", "Sales");
 manager.displayInfo();
+// ""Управление списком заказов""
+
+// Реализуйте класс Order (заказ), который имеет следующие свойства и методы:
+
+// Свойство orderNumber (номер заказа) - число, уникальный номер заказа.
+// Свойство products (продукты) - массив, содержащий список продуктов в заказе.
+// Метод addProduct(product) - принимает объект product и добавляет его в список продуктов заказа.
+// Метод getTotalPrice() - возвращает общую стоимость заказа, основанную на ценах продуктов.
+
+// // Пример использования класса
+// class Product {
+// constructor(name, price) {
+// this.name = name;
+// this.price = price;
+// }
+// }
+
+// const order = new Order(12345);
+
+// const product1 = new Product(""Phone"", 500);
+// order.addProduct(product1);
+
+// const product2 = new Product(""Headphones"", 100);
+// order.addProduct(product2);
+
+// console.log(order.getTotalPrice()); // Вывод: 600
+
+// Пример использования класса
+class Product {
+constructor(name, price) {
+this.name = name;
+this.price = price;
+}
+}
+class Order {
+    constructor(orderNumber){
+        this.orderNumber = orderNumber;
+        this.products = [];
+    }
+    addProduct(product){
+        this.products.push(product);
+    }
+    getTotalPrice(){
+        let totalprice = 0;
+        for(let product of this.products){
+            totalprice+=product.price
+        }
+        return totalprice;
+    }
+}
+const order = new Order(12345);
+const product1 = new Product("Phone", 500);
+order.addProduct(product1);
+const product2 = new Product("Headphones", 100);
+order.addProduct(product2);
+console.log(order.getTotalPrice()); // Вывод: 600
